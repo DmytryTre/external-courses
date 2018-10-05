32b4e20f5e5ec55c711c0e1f9d051a39d16cf6d4
DmytryTre <dmitrytre1993@gmail.com>
2018-10-05 20:45:16 +0300

    first commit

---
+++ src/ex2_js-basics-part2/task01.js
@@ -0,0 +1,12 @@
+
+function numberShow (a) {
+    if ((typeof a) = "sting" ) {
+        a = "sting"
+    } else if ((typeof a) = "number") {
+        a = "number"
+    } else {
+        a = 'undefined'
+    }
+} 
+
+console.log(a)
